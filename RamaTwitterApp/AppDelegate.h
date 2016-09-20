27969287ff39ca1c58f3df85292e73e1638cbadc
Rama Pillutla <ramat@yahoo-inc.com>
2016-09-19 22:44:59 -0700

    Initial Commit

---
+++ RamaTwitterApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RamaTwitterApp
+//
+//  Created by Rama Pillutla on 9/19/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RamaTwitterApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RamaTwitterApp
+//
+//  Created by Rama Pillutla on 9/19/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
